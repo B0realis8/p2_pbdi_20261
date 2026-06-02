085cc7aab6183ebb6fbd5760b3fea6830553ff05
Renan Antonio <renan.amsouza8@gmail.com>
2026-06-02 09:14:08 -0300

    feat(p2): tabela para importação dos dados

--- P2/script.sql
+++ P2/script.sql
@@ -1,3 +1,26 @@
+-- 1 Base de dados e criação de tabela
+-- A base a ser utilizada pode ser obtida a partir do link a seguir. Há também uma versão
+-- disponível no ambiente da disciplina. Utilize a que preferir.
+-- https://www.kaggle.com/datasets/csafrit2/higher-education-students-performance-eva
+-- luation
+-- Ela deve ser importada para uma base de dados gerenciada pelo PostgreSQL. Os dados
+-- devem ser armazenados em uma tabela apropriada para as análises desejadas. Você deve
+-- identificar as colunas necessárias, de acordo com a descrição de cada item da prova.
+-- Além, é claro, de uma chave primária (de auto incremento).
+
+
+CREATE TABLE estudantes(
+
+studentid SERIAL PRIMARY KEY,
+salary NUMERIC,
+mother_education INT,
+father_education INT,
+prep_study INT,
+prep_exam INT,
+grade INT
+
+)
+
 -- 2 Resultado em função da formação dos pais
 -- Escreva um stored procedure que exibe o número de alunos aprovados e cujos pais são
 -- ambos PhDs.
@@ -40,3 +63,7 @@ BEGIN
 END;
 $$;
 
+-- 4 Salário versus estudos
+-- Dentre os alunos que têm salário maior que 410, quantos costumam se preparar com
+-- frequência (regularmente) para os exames? Escreva um stored procedure que exibe esse
+-- valor.
